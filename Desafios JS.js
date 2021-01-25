5d85b714e926d0bc72889ef9e1b67bc95d2236fa
Camila-Santana <tst.santana.camila@gmail.com>
2021-01-25 10:53:15 -0300

    Create Desafios JS.js

---
+++ Desafios JS.js
@@ -0,0 +1,10 @@
+//Operadores Aritméticos numa só operação//
+
+var NumberOne = 10
+var NumberTwo = 5
+var NumberThree = 1
+
+
+var Resultado = (NumberOne + NumberTwo + NumberThree) / 2 * 5 ** 2 - 100
+
+console.log(Resultado)
